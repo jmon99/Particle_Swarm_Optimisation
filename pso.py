066442a56e6928e173cb0c015cfd4f3df668677e
Jed Arno <jedarno99@gmail.com>
2022-11-01 18:45:56 +0000

    Added swarm Initialisation
    Function or initialising velocity and positions of swarm added
    checks added for bounds being a numpy array of pairs
    if a python list object is given, it is converted to a nunmpy array
    positions and velocities are stored as a attribute in swary, as a numpy array

--- pso.py
+++ pso.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 """
 PSO class.
 """
@@ -15,6 +17,12 @@ class Swarm:
     max_iterations=200,
     cost_bound=None):
 
+    #check bounds is a numpy array of pairs
+    if type(bounds) != 'numpy.ndarray':
+      bounds = np.array(bounds)
+    if bounds.shape[1] != 2:
+      print("bounds should be a numpy  array of pairs of bounds in the format np.array([[x1_lower, x1_upper], [x2_lower, x2_upper], ...])")
+
     self.function = function
     self.population = population
     self.bounds = bounds
@@ -24,8 +32,14 @@ class Swarm:
     self.max_iterations = max_iterations
     self.cost_bound = cost_bound
     self.vmax = vmax
+    self.velocity = None
+    self.position = None
 
     
+  def initialise_swarm(self):
+    self.velocity = np.random.uniform(0 - self.vmax, self.vmax, self.population)
+    lower_bounds = self.bounds[:,0]
+    upper_bounds = self.bounds[:,1]
+    self.position = np.random.uniform(lower_bounds, upper_bounds)
 
-
-
+  
--- pso_test.py
+++ pso_test.py
@@ -1,4 +1,5 @@
 from pso import Swarm
+import numpy as np
 
 def line():
   print("--------------------------------------------------------------------------------------------------")
@@ -12,7 +13,7 @@ def test_init():
   line()
   print("Testing object and attribute initialisation...")
   swarm = Swarm(function=simple_fun, population = 50, bounds = [[0,1], [1,10]], vmax=1)
-  if (swarm.population==50 and swarm.bounds[1]==[1,10] and swarm.vmax==1 and swarm.c1==2.8 and swarm.c2==1.3 and swarm.beta==1 and swarm.max_iterations==200):
+  if (swarm.population==50 and swarm.bounds[1][1] == 10 and swarm.vmax==1 and swarm.c1==2.8 and swarm.c2==1.3 and swarm.beta==1 and swarm.max_iterations==200):
 
     print("Test Passed!")
     line()
@@ -22,13 +23,52 @@ def test_init():
   line()
   return False
 
-tests = [test_init]
+def test_initial_vel():
+  line()
+  print("Initial Velocity Test")
+  line()
+  bounds = np.array([[0,1], [1,10]])
+  population = 50
+  swarm = Swarm(function=simple_fun, population = population, bounds=bounds, vmax=1)
+  swarm.initialise_swarm()
+
+  if(swarm.velocity[0] > 1 or swarm.velocity[1] > 1):
+    line()
+    return "Test failed, initial velocity is too high :("
+
+  if(swarm.velocity[0] < -1 or swarm.velocity[1] < -1):
+    line()
+    return "Initial Velocity Test failed, initial velocity is too Low :("
+    return False
+
+  print("Test passed!")
+  line()
+  return True
+
+def test_initial_pos():
+  line()
+  print("Initial Position Test")
+  line()
+  bounds = np.array([[0,1], [1,10]])
+  population = 50
+  swarm = Swarm(function=simple_fun, population=population, bounds=bounds, vmax=1)
+  swarm.initialise_swarm()
+  
+  if(swarm.position[0] < 0 or swarm.position[1] < 1 or swarm.position[0] > 1 or swarm.position[1] > 10):
+    line()
+    return "Inital Position Test Failed, postion is outside of given bounds"
+
+  return True
+
+tests = [test_init, test_initial_vel, test_initial_pos]
 total_tests = len(tests)
 passes = 0
 
 for test in tests:
-  if test():
+  passed = test()
+  if passed == True:
     passes += 1
+  else: print(passed)
 
 line()
 print("Passed ",passes," out of",total_tests," tests!")
