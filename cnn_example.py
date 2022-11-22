9f5d98debee7eecd84d3994e659c15f126a42bf9
Jed Arno <jedarno99@gmail.com>
2022-11-22 11:10:41 +0000

    SGD example optimising lr and momentum

--- cnn_example.py
+++ cnn_example.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.cuda
@@ -6,7 +7,7 @@ from torch.optim import SGD,Adam,RMSprop
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
+from pso import Swarm
 
   
 class Cnn(nn.Module):
@@ -66,6 +67,7 @@ def test(dataloader, model, loss_fn):
   test_loss /= num_batches
   correct /= size
   print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+  return test_loss
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
@@ -92,11 +94,29 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 epochs = 5
 
+"""
 for t in range(epochs):
+  print(f"Epoch {t+1}\n-------------------------------")
+  train(train_dataloader, model, loss_fn, optimizer)
+  loss = test(test_dataloader, model, loss_fn)
+print("Done!")
+"""
+
+def pso_wrapper(params):
+  print(params)
+  optimizer = torch.optim.SGD(model.parameters(), lr=params[0])
+  
+  for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    loss = test(test_dataloader, model, loss_fn)
+  
+  return(loss)
 
-print("Done!")
+swarm = Swarm(pso_wrapper, 5, [[0,0.1]], 0.75, beta=0.8)
+swarm.initialise_swarm()
+swarm.fit(tol=0.001)
+print(swarm.g_best)
+print(swarm.g_fitness)
 
 
