8fb5a67e5fdb0d856c911994b3fc7a30aeeab4e8
Jed Arno <jedarno99@gmail.com>
2022-11-22 14:48:22 +0000

    GPU use and swarm generation

--- cnn_example.py
+++ cnn_example.py
@@ -9,7 +9,17 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 from pso import Swarm
 
-  
+for i in range(torch.cuda.device_count()):
+  print("device: {}".format(torch.cuda.get_device_name(i)))
+
+device = "cuda:3" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+print(device)
+
+def weight_reset(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        m.reset_parameters()
+
 class Cnn(nn.Module):
 
   def __init__(self):
@@ -22,16 +32,17 @@ class Cnn(nn.Module):
     self.fc3 = nn.Linear(84, 10)
 
   def forward(self, x):
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
+    x = self.pool(torch.relu(self.conv1(x)))
+    x = self.pool(torch.relu(self.conv2(x)))
     x = torch.flatten(x, 1)
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
+    x = torch.relu(self.fc1(x))
+    x = torch.relu(self.fc2(x))
     return x
 
 
 def train(dataloader, model, loss_fn, optimizer):
   size = len(dataloader.dataset)
+  model.to(device)
   model.train(mode=True)
 
   for batch, (X,y) in enumerate(dataloader):
@@ -53,13 +64,14 @@ def train(dataloader, model, loss_fn, optimizer):
 def test(dataloader, model, loss_fn):
   size = len(dataloader.dataset)
   num_batches = len(dataloader)
+  model.to(device)
   model.eval()
   test_loss, correct = 0, 0
 
   with torch.no_grad():
 
     for X, y in dataloader:
-      X.to(device), y.to(device)
+      X,y = X.to(device), y.to(device)
       pred = model(X)
       test_loss += loss_fn(pred, y).item()
       correct += (pred.argmax(1) == y).type(torch.float).sum().item()
@@ -68,9 +80,6 @@ def test(dataloader, model, loss_fn):
   correct /= size
   print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
   return test_loss
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
 
 training_data = datasets.FashionMNIST(
   root="data",
@@ -104,19 +113,20 @@ print("Done!")
 
 def pso_wrapper(params):
   print(params)
-  optimizer = torch.optim.SGD(model.parameters(), lr=params[0])
+  model = Cnn().to(device)
+  optimizer = torch.optim.SGD(model.parameters(), lr=params[0], momentum=params[1])
   
   for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
     loss = test(test_dataloader, model, loss_fn)
+    
+  torch.cuda.empty_cache(),
   
   return(loss)
 
-swarm = Swarm(pso_wrapper, 5, [[0,0.1]], 0.75, beta=0.8)
+swarm = Swarm(pso_wrapper, 2, [[0,0.1], [0, 1]], 0.412, beta=0.8)
 swarm.initialise_swarm()
 swarm.fit(tol=0.001)
 print(swarm.g_best)
 print(swarm.g_fitness)
-
-
