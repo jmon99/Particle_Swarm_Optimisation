bea03a63dfbac03053b45ea08b681498c5996e40
Jed Arno <ZFAC230@CIM-STF-00694.local>
2022-12-19 18:04:19 +0000

    Merging firefly oprimiser into repo

---
+++ firefly.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def convert_to_bounds(bounds, positions):
+  values = np.zeros_like(positions)
+  
+  for i, pos in enumerate(positions):
+    for j in range(len(bounds)):
+      values[i][j] = bounds[j][0] + ((bounds[j][1] - bounds[j][0]) * pos[j])
+  
+  return values
+
+def distance(i, j):
+  return np.linalg.norm(i - j)
+
+class Firefly:
+
+  def __init__(self,
+    function,
+    population,
+    bounds,
+    beta_0,
+    light_absorption,
+    randomisation_param,
+    final_randomisation_param = None):
+
+    self.function = function
+    self.population = population
+    self.limits = bounds
+    self.bounds = np.array([[0,1]] * len(bounds))
+    self.beta_0 = beta_0
+    self.gamma = light_absorption
+    self.alpha = randomisation_param
+    self.alpha_inf = final_randomisation_param
+    self.position = None
+    self.attractiveness = None
+    self.g_fitness = None
+    self.g_best = None
+    self.cur_fitness = None
+   
+  def initialise_swarm(self):
+    lower_bounds = self.bounds[:,0]
+    upper_bounds = self.bounds[:,1]
+    self.position = np.random.uniform(lower_bounds, upper_bounds, [self.population, len(lower_bounds)])
+    params = convert_to_bounds(self.limits, self.position)
+    self.attractiveness = np.fromiter(map(self.function, params), dtype=np.float32)
+    self.g_fitness = self.attractiveness.min()
+    self.g_best = self.position[np.argmin(self.attractiveness)]
+
+  def step(self, steps=1):
+
+    for step in range(steps):
+      #update randomisation parameter
+      self.alpha *= 0.98
+
+      for i,pos in enumerate(self.position):
+
+        for j,firefly in enumerate(self.position):
+
+          if self.attractiveness[j] < self.attractiveness[i]:
+            exponent = -self.gamma * distance(i,j)**2
+            add = np.array(self.beta_0 * np.exp(exponent) * (firefly - pos) + self.alpha*np.random.uniform(-0.5,0.5,len(self.bounds)))
+            self.position[i] = self.position[i] + add 
+      params = convert_to_bounds(self.limits, self.position)
+      self.attractiveness = np.fromiter(map(self.function, params), dtype=np.float32)
+      g_fitness = self.attractiveness.min()
+      if g_fitness < self.g_fitness:
+        self.g_fitness = g_fitness
+
+      print("step{}: swarm fitness = {}".format(step, g_fitness))
+
+
---
+++ test_firefly.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+from firefly import Firefly, distance
+
+def sum_fun(param):
+  sum  = 0
+
+  for num in param:
+    sum += num
+
+  return sum
+
+def rosenbrock(param):
+  f = 0
+
+  for i in range(len(param) - 1):
+    f += (100 * (param[i + 1] - param[i]**2)**2 + (1 - param[i])**2)
+
+  return f
+
+
+def test_init():
+  swarm = Firefly(function = rosenbrock, population = 10, bounds= np.array([[-5,5],[-5,5]]), beta_0 = 1 ,light_absorption = 0.5, randomisation_param = 0.5)
+  expected_search_bounds = np.array([[0, 1], [0, 1]])
+  expected_parameter_bounds = np.array([[-5, 5], [-5, 5]])
+
+  for i in range(len(expected_search_bounds)):
+    if expected_search_bounds[i][0] != swarm.bounds[i][0] or swarm.bounds[i][1] != swarm.bounds[i][1]:
+      return "incorrect bounds on search space. recieved {}, expected {}".format(swarm.bounds, expected_search_bounds)
+
+  for i in range(len(expected_parameter_bounds)):
+    if expected_parameter_bounds[i][0] !=  swarm.limits[i][0] or expected_parameter_bounds[i][1] != swarm.limits[i][1]:
+      return "incorrect bounds on parameter space. recieved {}, expected{}".format(swarm.limits, expected_parameter_bounds)
+
+  return True
+
+def test_initialise_swarm():
+  swarm = Firefly(function=sum_fun, population = 1, bounds = np.array([[0,10], [0,10]]), beta_0 = 1, light_absorption = 0.5, randomisation_param = 0.5)
+  swarm.initialise_swarm()
+
+  for i, pos in enumerate(swarm.position):
+
+    if (swarm.attractiveness[i], 2) != ((pos[0] + pos[1]) * 10, 2):
+      return "Attractiveness calculated incorectly, should use fitness function. recieved {}, expected {}".format((pos[0] + pos[1])*10, swarm.attractiveness[i])
+
+  print("pos: ", swarm.position)
+  return True
+
+def test_distance():
+  i = np.array([0.5, 1, 0.3])
+  j = np.array([0.2, 0.9, 0.4])
+  expected = 0.3316625
+  result = distance(i, j)
+  result = np.round(result, 7)
+
+  if expected != result:
+    return "incorrect distance, ecpected {}, but recieved {}".format(expected, result)
+
+  return True
+
+def test_step():
+  swarm = Firefly(function=sum_fun, population = 30, bounds = np.array([[0,1],[0,1]]),beta_0 = 1 ,light_absorption=0.01, randomisation_param = 1)
+  swarm.position = np.array([[0,0],[0.5,0.5]])
+  swarm.attractiveness = np.array([0,1])
+  swarm.step(steps = 2)
+  print(swarm.position)
+  print(swarm.attractiveness)
+  return True
+
+def test_rosenbrock():
+  bounds = np.array([[-5, 5]]*30) 
+  population = 30
+  swarm = Firefly(function = rosenbrock, population = population, bounds = bounds, beta_0 = 1, light_absorption=0.01, randomisation_param=1)
+  swarm.initialise_swarm()
+  swarm.step(steps = 500)
+
