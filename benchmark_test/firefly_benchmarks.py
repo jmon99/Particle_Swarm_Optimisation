58ac35a64a000277c7106fa863b6aec91bc28299
Jed Arno <ZFAC230@CIM-STF-00694.local>
2023-01-04 20:18:54 +0000

    Firefly switch benchmarks

--- benchmark_test/firefly_benchmarks.py
+++ benchmark_test/firefly_benchmarks.py
@@ -4,7 +4,12 @@ import numpy as np
 from inspect import signature
 from firefly import Firefly
 
+#test parameters
 TEST_NUM = 100
+pop=30
+beta_0=1.5
+gamma=0.8
+alpha=1
 
 def line():
   print(u'\u2500' * 50)
@@ -28,7 +33,7 @@ for index, func in enumerate(bf.BenchmarkFunction.__subclasses__()):
     bounds = bounds.T
     print(bounds)
 
-    swarm = Firefly(function=instance, population=30, bounds=bounds, beta_0=1, light_absorption=0.4, randomisation_param = 1)
+    swarm = Firefly(function=instance, population=pop, bounds=bounds, beta_0=beta_0, light_absorption=gamma, randomisation_param = alpha)
     
     for i in range(100):
       swarm.initialise_swarm()
@@ -41,9 +46,21 @@ average = np.mean(results, axis=1)
 minimum = results.min(axis=1)
 maximum = results.max(axis=1)
 std = np.std(results, axis=1)
+
 print("mean: ", average)
 print("min: ", minimum)
 print("max: ",  maximum)
 print("std: ", std)
 
+print("swarm size={}".format(pop))
+print("firefly: beta_0={}, gamma={}, alpha={}".format(beta_0, gamma, alpha))
+print(" Func | avrg | min  | max  | std  |")
+
+for index, func in enumerate(bf.BenchmarkFunction.__subclasses__()):
+  instance=func()
+  iavg = np.format_float_scientific(average[index], precision=4)
+  imin = np.format_float_scientific(minimum[index], precision=4)
+  imax = np.format_float_scientific(maximum[index], precision=4)
+  istd = np.format_float_scientific(std[index], precision=4)
+  print("{} & {} & {} & {} & {} ".format(instance.name(), iavg, imin, imax, istd))
 
